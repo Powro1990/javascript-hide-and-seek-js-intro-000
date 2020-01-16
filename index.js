356c148c81179e604104a73deeddc9b95c544499
Paweł Powroźnik <pawel.powroznik21@gmail.com>
2020-01-16 20:39:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,6 +14,6 @@ function deepestChild(){
 function increaseRankBy(n){
   var list = document.getElementById('app').querySelectorAll('ul.ranked-list');
   
-  return parseInt(list[0].innerHTML)
+  return parseInt(list[0].innerHTML) + n
   
 }
