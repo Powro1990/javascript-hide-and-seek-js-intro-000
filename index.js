487794a8f4b26fd56e9cfbb95e89b46a7e058a29
Paweł Powroźnik <pawel.powroznik21@gmail.com>
2020-01-16 18:25:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ function nestedTarget(){
 }
 
 function deepestChild(){
-  var doku = document.getElementById('grand-node').querySelector('div');
+  var doku = []
+  do
   return doku.querySelector('div');
 }
