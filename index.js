71e58968010e8f1e193a663c057c79130c87798c
Paweł Powroźnik <pawel.powroznik21@gmail.com>
2020-01-16 19:26:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function deepestChild(){
 function increaseRankBy(n){
   var list = document.querySelector("ul.ranked-list li");
   for (let i = 0; i< list.length; i++){
-    list[i].innerHTML + n
+    list[i].innerHTML = 
   }
   return list;
 }
